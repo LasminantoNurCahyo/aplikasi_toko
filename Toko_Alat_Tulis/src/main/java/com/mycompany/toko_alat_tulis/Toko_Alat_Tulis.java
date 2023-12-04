f83a3c7b906c1979ad742283e64dac544cfa401e
LasminantoNurCahyo <112052843+LasminantoNurCahyo@users.noreply.github.com>
2023-12-04 22:17:50 +0700

    Add files via upload

---
+++ Toko_Alat_Tulis/src/main/java/com/mycompany/toko_alat_tulis/Toko_Alat_Tulis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.toko_alat_tulis;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Toko_Alat_Tulis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
